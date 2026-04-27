a61f717e14028d52f94373dcba4f3d18ec8e717a
Artur <ka57748@zut.edu.pl>
2026-04-27 13:30:45 +0200

    lab06

--- LAB6TD/czesc2/src/main.rs
+++ LAB6TD/czesc2/src/main.rs
@@ -1,5 +1,5 @@
 use plotters::prelude::*;
-use rustfft::{num_complex::Complex, FftPlanner};
+use rustfft::{FftPlanner, num_complex::Complex};
 use std::f64::consts::PI;
 
 fn string_to_bits(text: &str) -> Vec<u8> {
@@ -41,7 +41,7 @@ fn calculate_spectrum_db(signal: &[f64]) -> Vec<f64> {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let text = "Test";
+    let text = "t";
     let bit_stream = string_to_bits(text);
 
     let w: f64 = 2.0;
@@ -88,9 +88,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mp_db = calculate_spectrum_db(&zp_vals);
 
     let n = za_vals.len();
-    let mut freqs = Vec::with_capacity(n / 2);
-    for i in 0..(n / 2) {
-        freqs.push((i as f64) * fs / (n as f64));
+    let mut freqs = Vec::new();
+    let mut ma_filtered = Vec::new();
+    let mut mf_filtered = Vec::new();
+    let mut mp_filtered = Vec::new();
+
+    for i in 1..(n / 2) {
+        let f = (i as f64) * fs / (n as f64);
+        freqs.push(f);
+        ma_filtered.push(ma_db[i]);
+        mf_filtered.push(mf_db[i]);
+        mp_filtered.push(mp_db[i]);
     }
 
     let root = BitMapBackend::new("widma_amplitudowe.png", (1024, 768)).into_drawing_area();
@@ -98,51 +106,93 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let areas = root.split_evenly((3, 1));
 
-    let ma_series: Vec<(f64, f64)> = freqs.iter().cloned().zip(ma_db.iter().cloned()).collect();
-    let mf_series: Vec<(f64, f64)> = freqs.iter().cloned().zip(mf_db.iter().cloned()).collect();
-    let mp_series: Vec<(f64, f64)> = freqs.iter().cloned().zip(mp_db.iter().cloned()).collect();
+    let ma_series: Vec<(f64, f64)> = freqs
+        .iter()
+        .cloned()
+        .zip(ma_filtered.iter().cloned())
+        .collect();
+    let mf_series: Vec<(f64, f64)> = freqs
+        .iter()
+        .cloned()
+        .zip(mf_filtered.iter().cloned())
+        .collect();
+    let mp_series: Vec<(f64, f64)> = freqs
+        .iter()
+        .cloned()
+        .zip(mp_filtered.iter().cloned())
+        .collect();
 
-    let max_freq = 100.0;
+    let max_f = fs / 2.0;
+    let bottom_db = -100.0f64;
 
     let mut chart_ma = ChartBuilder::on(&areas[0])
         .margin(10)
         .caption("Widmo amplitudowe ASK [dB]", ("sans-serif", 20))
         .x_label_area_size(30)
         .y_label_area_size(40)
-        .build_cartesian_2d(0f64..max_freq, -100.0f64..0.0f64)?;
+        .build_cartesian_2d((1f64..max_f).log_scale(), bottom_db..0.0f64)?;
     chart_ma
         .configure_mesh()
-        .x_desc("Czestotliwosc [Hz]")
+        .x_desc("Czestotliwosc [Hz] (log)")
         .y_desc("Amplituda [dB]")
         .draw()?;
-    chart_ma.draw_series(LineSeries::new(ma_series, &BLUE))?;
+
+    chart_ma.draw_series(
+        ma_series
+            .iter()
+            .map(|&(x, y)| PathElement::new(vec![(x, bottom_db), (x, y)], &BLUE)),
+    )?;
+    chart_ma.draw_series(
+        ma_series
+            .iter()
+            .map(|&(x, y)| Circle::new((x, y), 2, BLUE.filled())),
+    )?;
 
     let mut chart_mf = ChartBuilder::on(&areas[1])
         .margin(10)
         .caption("Widmo amplitudowe FSK [dB]", ("sans-serif", 20))
         .x_label_area_size(30)
         .y_label_area_size(40)
-        .build_cartesian_2d(0f64..max_freq, -100.0f64..0.0f64)?;
+        .build_cartesian_2d((1f64..max_f).log_scale(), bottom_db..0.0f64)?;
     chart_mf
         .configure_mesh()
-        .x_desc("Czestotliwosc [Hz]")
+        .x_desc("Czestotliwosc [Hz] (log)")
         .y_desc("Amplituda [dB]")
         .draw()?;
-    chart_mf.draw_series(LineSeries::new(mf_series, &RED))?;
+
+    chart_mf.draw_series(
+        mf_series
+            .iter()
+            .map(|&(x, y)| PathElement::new(vec![(x, bottom_db), (x, y)], &RED)),
+    )?;
+    chart_mf.draw_series(
+        mf_series
+            .iter()
+            .map(|&(x, y)| Circle::new((x, y), 2, RED.filled())),
+    )?;
 
     let mut chart_mp = ChartBuilder::on(&areas[2])
         .margin(10)
         .caption("Widmo amplitudowe PSK [dB]", ("sans-serif", 20))
         .x_label_area_size(30)
         .y_label_area_size(40)
-        .build_cartesian_2d(0f64..max_freq, -100.0f64..0.0f64)?;
+        .build_cartesian_2d((1f64..max_f).log_scale(), bottom_db..0.0f64)?;
     chart_mp
         .configure_mesh()
-        .x_desc("Czestotliwosc [Hz]")
+        .x_desc("Czestotliwosc [Hz] (log)")
         .y_desc("Amplituda [dB]")
         .draw()?;
-    chart_mp.draw_series(LineSeries::new(mp_series, &GREEN))?;
 
+    chart_mp.draw_series(
+        mp_series
+            .iter()
+            .map(|&(x, y)| PathElement::new(vec![(x, bottom_db), (x, y)], &GREEN)),
+    )?;
+    chart_mp.draw_series(
+        mp_series
+            .iter()
+            .map(|&(x, y)| Circle::new((x, y), 2, GREEN.filled())),
+    )?;
     root.present()?;
     Ok(())
 }
